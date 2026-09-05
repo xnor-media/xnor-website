9c6bf12f945bc40b7c7a799149ad605c05681e9f
Pahan Supurna <pahansupurna2006@gmail.com>
2026-09-05 10:14:02 +0530

    Fixed @/ alias for all imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Services from "../components/Services";
-import ImpactBanner from "../components/ImpactBanner";
-import Showcase from "../components/Showcase";
-import CtaSection from "../components/CtaSection";
-import Footer from "../components/Footer";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import ImpactBanner from "@/components/ImpactBanner";
+import Showcase from "@/components/Showcase";
+import CtaSection from "@/components/CtaSection";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
