a5c24cae7fa18999e1303cc2b06e113afd45d156
Pahan Supurna <pahansupurna2006@gmail.com>
2026-09-25 20:48:25 +0530

    added a new reel

--- src/lib/reelsData.ts
+++ src/lib/reelsData.ts
@@ -24,6 +24,7 @@ export const reels: ReelItem[] = [
   { youtubeId: "kXaRtmI8U-s", title: "Reel 1" },
   // { youtubeId: "wFCuNKnFv_U", title: "Reel 2" },
   { youtubeId: "c12FY7deYIQ", title: "Reel 3" },
+  { youtubeId: "g1pNCTHSPfI", title: "Reel 19" },
   { youtubeId: "Bmtd0uyuzU8", title: "Reel 4" },
   { youtubeId: "ZDcSbwnGsm0", title: "Reel 5" },
   { youtubeId: "3bDWrOeNyfE", title: "Reel 6" },
@@ -39,4 +40,5 @@ export const reels: ReelItem[] = [
   { youtubeId: "yV9TAmrI-2k", title: "Reel 16" },
   { youtubeId: "L5KWE4dyWHc", title: "Reel 17" },
   { youtubeId: "-XHOMkdcSQU", title: "Reel 18" },
+ 
 ];
